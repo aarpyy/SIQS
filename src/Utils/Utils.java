6ef3c3e8811c0a810a29cadc1b080223a0227cea
aarpyy <acarpent@oberlin.edu>
2022-02-07 10:48:30 -0500

    working modSqrt and BigInteger sqrt

--- src/Utils/Utils.java
+++ src/Utils/Utils.java
@@ -1,9 +1,11 @@
 package Utils;
 
-import QS.IntArray;
 import QS.BigIntArray;
+import QS.IntArray;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.MathContext;
 import java.util.Random;
 
 public final class Utils {
@@ -11,15 +13,31 @@ public final class Utils {
     public static final BigInteger TWO = BigInteger.valueOf(2);
     public static final BigInteger THREE = BigInteger.valueOf(3);
 
+    /*
+    Helper function that divides n by the factor until it can't divide anymore, returning
+    the result after division and the number of times division was performed as a Pair
+     */
+    public static Pair<BigInteger, Integer> removeFactor(BigInteger n, BigInteger factor) {
+        BigInteger[] div;
+        int factorCount = 0;
+        while ((div = n.divideAndRemainder(factor))[1].equals(BigInteger.ZERO)) {
+            n = div[0];
+            factorCount++;
+        }
+        return new Pair<>(n, factorCount);
+    }
+
+    /*
+    Attempts to completely factor n using the given factor base, returning the powers of the factors
+    if number was completely factored, throwing ArithmeticException if not
+     */
     public static IntArray smoothFactor(BigInteger n, BigIntArray primes) throws ArithmeticException {
         int[] factors = new int[primes.length];
-        BigInteger[] div;
+        Pair<BigInteger, Integer> factor;
         for (int i = 0; i < primes.length; i++) {
-            factors[i] = 0;
-            while ((div = n.divideAndRemainder(primes.get(i)))[1].equals(BigInteger.ZERO)) {
-                n = div[0];
-                factors[i]++;
-            }
+            factor = removeFactor(n, primes.get(i));
+            n = factor.first();
+            factors[i] = factor.second();
         }
 
         if (n.equals(BigInteger.ONE)) {
@@ -29,6 +47,10 @@ public final class Utils {
         }
     }
 
+    /*
+    Returns boolean of if number can be factored by factor base. This is identical in
+    function to attempting smoothFactor and returning false if error thrown, otherwise true
+     */
     public static boolean smoothQ(BigInteger n, BigIntArray primes) {
         BigInteger[] div;
         for (BigInteger p : primes) {
@@ -59,6 +81,10 @@ public final class Utils {
         }
     }
 
+    /*
+    Performs fast power algorithm with BigInteger as a power (as opposed to BigIntegers'
+    usual .pow() method which only accepts integer exponents
+     */
     public static BigInteger fastPower(BigInteger a, BigInteger p) {
         BigInteger result = BigInteger.ONE;
         while (!p.equals(BigInteger.ZERO)) {
@@ -72,23 +98,39 @@ public final class Utils {
         return result;
     }
 
+    /*
+    Returns a random BigInteger >= l and < u. Essentially Python's randrange(l, u)
+     */
     public static BigInteger randRange(BigInteger l, BigInteger u, Random rand) {
-        int bits = u.bitLength();
+        /*
+        Subtract lower limit from upper limit so that we know the actual range that we
+        have to generate the random BigInteger
+         */
+        BigInteger range = u.subtract(l);
+        int bits = range.bitLength();
 
         // Creates new random BigInteger with bits as the number of bits
         BigInteger result = new BigInteger(bits, rand);
 
         /*
-        Since(TWO numbers can have the same number of bits but be not equal,
-        check to see if this number is less than upper limit in addition to greater
-        than lower limit before returning.
+        While it's greater, get a new one, then we guarantee we have a number that is within
+        the designated range. Re-add lower limit so that the range isn't 0-range but is
+        l-u and then return
          */
-        while ((result.compareTo(l) < 0) || (result.compareTo(u) >= 0)) {
+        while (result.compareTo(range) >= 0) {
             result = new BigInteger(bits, rand);
         }
-        return result;
+        return result.add(l);
     }
 
+    public static BigInteger sqrt(BigInteger a) {
+        BigDecimal d = new BigDecimal(a);
+        return d.sqrt(MathContext.UNLIMITED).toBigInteger();
+    }
+
+    /*
+    Returns the modular square root of a mod p, throwing ArithmeticException if none exist
+     */
     public static BigInteger modSqrt(BigInteger a, BigInteger p) throws ArithmeticException {
         if (!quadraticResidue(a, p)) {
             throw new ArithmeticException(a + " does not have a quadratic residue mod " + p);
@@ -97,13 +139,9 @@ public final class Utils {
         if (p.and(THREE).equals(THREE)) {
             return a.modPow(p.add(BigInteger.ONE).shiftRight(2), p);
         } else {
-            BigInteger[] div;
-            BigInteger Q = p.subtract(BigInteger.ONE);
-            int S = 0;
-            while ((div = Q.divideAndRemainder(TWO))[1].equals(BigInteger.ZERO)) {
-                Q = div[0];
-                S++;
-            }
+            Pair<BigInteger, Integer> div = removeFactor(p.subtract(BigInteger.ONE), TWO);
+            BigInteger Q = div.first();
+            int S = div.second();
 
             Random rand = new Random();
             BigInteger z = randRange(TWO, p, rand);
@@ -147,14 +185,20 @@ public final class Utils {
         }
     }
 
-    public static boolean quadraticResidue(BigInteger a, BigInteger n) {
+    /*
+    Returns whether a number c exists s.t. c^2 = a mod p
+     */
+    public static boolean quadraticResidue(BigInteger a, BigInteger p) {
         // Returns a ^ ((p - 1) / 2) == 1, which tells us if there exists an integer c s.t.
-        // c^2 = a mod n
-        return a.modPow(n.subtract(BigInteger.ONE).shiftRight(1), n).equals(BigInteger.ONE);
+        // c^2 = a mod p
+        return a.modPow(p.subtract(BigInteger.ONE).shiftRight(1), p).equals(BigInteger.ONE);
     }
 
-    public static boolean quadraticNonResidue(BigInteger a, BigInteger n) {
-        BigInteger nSub1 = n.subtract(BigInteger.ONE);
-        return a.modPow(nSub1.shiftRight(1), n).equals(nSub1);
+    /*
+    Returns whether a number c does NOT exist s.t. c^2 = a mod p
+     */
+    public static boolean quadraticNonResidue(BigInteger a, BigInteger p) {
+        BigInteger nSub1 = p.subtract(BigInteger.ONE);
+        return a.modPow(nSub1.shiftRight(1), p).equals(nSub1);
     }
 }
--- test/Utils/UtilsTest.java
+++ test/Utils/UtilsTest.java
@@ -3,11 +3,16 @@ package Utils;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UtilsTest {
+
+    private static final boolean print = false;
 
     @Test
     void smoothFactor() {
@@ -27,6 +32,44 @@ class UtilsTest {
 
     @Test
     void randRange() {
+        int u = 20;
+        int l = 10;
+
+        BigInteger upper = BigInteger.valueOf(u);
+        BigInteger lower = BigInteger.valueOf(l);
+        Random rand = new Random();
+
+        BigInteger r;
+
+        int range = u - l;
+        int[] generated = new int[range];
+        for (int i = 0; i < range; i++) {
+            generated[i] = 0;
+        }
+
+        int total = 10000;
+        for (int i = 0; i < total; i++) {
+            r = Utils.randRange(lower, upper, rand);
+            generated[r.intValue() - l]++;
+
+            // Confirm that all numbers are within the range
+            assertTrue(r.compareTo(lower) >= 0);
+            assertTrue(r.compareTo(upper) < 0);
+        }
+
+        // Check that all numbers within the range were generated at least once
+        for (int i : generated) {
+            assertTrue(i > 0);
+        }
+
+        if (print) {
+            System.out.println("Distribution: " + Arrays.toString(generated));
+            float[] percent = new float[range];
+            for (int i = 0; i < range; i++) {
+                percent[i] = (generated[i] / (float) total) * 100;
+            }
+            System.out.println("Percent: " + Arrays.toString(percent));
+        }
     }
 
     @Test
@@ -46,8 +89,11 @@ class UtilsTest {
         for (BigInteger n : squares) {
             try {
                 sq = Utils.modSqrt(n, p);
-                // System.out.println("N: " + n + "; √n: " + sq + " ^2: " + sq.modPow(Utils.TWO, p));
                 assertEquals(sq.modPow(Utils.TWO, p), n);
+
+                if (print) {
+                    System.out.println("N: " + n + "; √n: " + sq + " ^2: " + sq.modPow(Utils.TWO, p));
+                }
             } catch (ArithmeticException e) {
                 System.out.println(n + " does not have a square root mod " + p);
             }
